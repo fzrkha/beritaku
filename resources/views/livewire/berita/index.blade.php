2c667f4961d84ca9222f169010557aee7e6ed6cb
fzrkha <redfanz02@gmail.com>
2023-09-09 12:42:49 +0700

    sudah benar

--- resources/views/livewire/berita/index.blade.php
+++ resources/views/livewire/berita/index.blade.php
@@ -1,40 +1,42 @@
 @section('title')
 Beritaku - HOME
 @endsection
-<div id="content">
-    <div id="single">
-          <div id="featured-image" style="background:url(images/bikeTW.jpg)">&nbsp;</div>
-         <h2>Penemuan Telegram Kabarkan Dahsyatnya Letusan Krakatau</h2>
-         KOMPAS.com - Dibandingkan letusan Gunung Tambora di Sumbawa (Nusa
-        Tenggara Timur) pada 1815, letusan Gunung Krakatau masih kalah besar, baik kekuatan maupun dampaknya. Berbeda dengan letusan Tambora yang terekam samar dan dampak globalnya baru dideteksi lebih dari 100 tahun kemudian, letusan Krakatau diketahui warga dunia dalam bilangan jam [...]
-         <div id="more"><a href="article-1.html">Baca Selengkapnya</a></div>
+<div>
+    <div id="content">
+        <div id="single">
+            <div id="featured-image" style="background:url({{ asset('img') }}/bikeTW.jpg)">&nbsp;</div>
+                <h2>Penemuan Telegram Kabarkan Dahsyatnya Letusan Krakatau</h2>
+                KOMPAS.com - Dibandingkan letusan Gunung Tambora di Sumbawa (Nusa Tenggara Timur) pada 1815, letusan Gunung Krakatau masih kalah besar, baik kekuatan maupun dampaknya. Berbeda dengan letusan Tambora yang terekam samar dan dampak globalnya baru dideteksi lebih dari 100 tahun kemudian, letusan Krakatau diketahui warga dunia dalam bilangan jam [...]
+                <div id="more"><a href="article-1.html">Baca Selengkapnya</a></div>
+            </div>
+            <div id="single">
+                <div id="featured-image" style="background:url(images/2.jpg)">&nbsp;</div>
+                <h2>Pesawat Tanpa Awak UGM Berdaya Jelajah 200 Kilometer</h2>
+                JAKARTA, KOMPAS.com - Untuk memantau batas wilayah atau situasi dan kondisi lokasi bencana alam dengan biaya murah dan efektif, dibutuhkan teknologi pesawat tanpa awak. Universitas Gadjah Mada (UGM) turut memamerkan hasil risetnya, berupa pesawat udara tanpa awak mini (Mini UAV) pada Forum Riset Industri Indonesia ke-3 2011, Rabu (30/11/2011) di [...]
+                <div id="more"><a href="#">Baca Selengkapnya</a></div>
+            </div>
+        </div>
+        <div id="sidebar">
+            <li class="widget">
+                <h2>Popular Post</h2>
+                <ul>
+                    <li><a href="#">Prasasti Maya Bukan Petunjuk Kiamat</a></li>
+                    <li><a href="#">Asteroid Vesta dalam Video 3D</a></li>
+                    <li><a href="#">Seperlima Gletser Himalaya Mencair</a></li>
+                </ul>
+            </li>
+            <li class="widget">
+                <h2>Commented Post</h2>
+                <ul>
+                    <li><a href="#">Prasasti Maya Bukan Petunjuk Kiamat</a></li>
+                    <li><a href="#">Asteroid Vesta dalam Video 3D</a></li>
+                    <li><a href="#">Seperlima Gletser Himalaya Mencair</a></li>
+                </ul>
+            </li>
+            <li class="widget">
+                  <h2>Audio Streaming</h2>
+                  <audio id="audio" src="sounds/sound.mp3" controls></audio>
+            </li>
+        </div>
     </div>
-    <div id="single">
-          <div id="featured-image" style="background:url(images/2.jpg)">&nbsp;</div>
-         <h2>Pesawat Tanpa Awak UGM Berdaya Jelajah 200 Kilometer</h2>
-         JAKARTA, KOMPAS.com - Untuk memantau batas wilayah atau situasi dan kondisi lokasi bencana alam dengan biaya murah dan efektif, dibutuhkan teknologi pesawat tanpa awak. Universitas Gadjah Mada (UGM) turut memamerkan hasil risetnya, berupa pesawat udara tanpa awak mini (Mini UAV) pada Forum Riset Industri Indonesia ke-3 2011, Rabu (30/11/2011) di [...]
-     <div id="more"><a href="#">Baca Selengkapnya</a></div>
-    </div>
-</div>
-<div id="sidebar">
-    <li class="widget">
-      <h2>Popular Post</h2>
-          <ul>
-            <li><a href="#">Prasasti Maya Bukan Petunjuk Kiamat</a></li>
-            <li><a href="#">Asteroid Vesta dalam Video 3D</a></li>
-            <li><a href="#">Seperlima Gletser Himalaya Mencair</a></li>
-          </ul>
-    </li>
-    <li class="widget">
-          <h2>Commented Post</h2>
-          <ul>
-            <li><a href="#">Prasasti Maya Bukan Petunjuk Kiamat</a></li>
-            <li><a href="#">Asteroid Vesta dalam Video 3D</a></li>
-            <li><a href="#">Seperlima Gletser Himalaya Mencair</a></li>
-          </ul>
-    </li>
-    <li class="widget">
-          <h2>Audio Streaming</h2>
-          <audio id="audio" src="sounds/sound.mp3" controls></audio>
-    </li>
 </div>
